e0fdf7caddd21114601a4e7416b5e36c2fb892c6
Riceman2000 <alex4222000@gmail.com>
2023-07-03 12:18:57 -0400

    Style tweaks

--- src/main.rs
+++ src/main.rs
@@ -5,64 +5,48 @@ use std::io::prelude::*;
 use std::path::Path;
 
 fn main() {
-    let scale_factor: f32 = 1.0;
-    let in_path: String = "examples/test03.png".to_owned();
+    let scale_factor: f32 = 0.25;
+    let in_path: String = "examples/test01.jpg".to_owned();
     let out_path: String = format!("{}{}", in_path, ".txt");
 
-    let img = image::open(in_path);
-    let img: image::ImageBuffer<image::Rgb<u8>, Vec<u8>> = match img {
-        Ok(image) => image.to_rgb8(),
-        Err(error) => {
-            println!("Error opening image: {}", error);
-            return;
-        }
-    };
+    let img = image::open(in_path)
+        .expect("Error reading image file")
+        .to_rgb8();
 
     println!("dimensions: {:?}", img.dimensions());
 
     let (x_block, y_block) = calculate_block_sizes(scale_factor);
     println!("Sub image size: {},{}", x_block, y_block);
 
-    let processed_img = process_image(x_block, y_block, &img);
-    let processed_img = match processed_img {
-        Ok(text) => text,
-        Err(error) => {
-            println!("Failed with error: {}", error);
-            return;
-        }
-    };
-    match write_output(&out_path, &processed_img) {
-        Ok(_) => {}
-        Err(error) => {
-            println!("Failed to write output file: {}", error);
-        }
-    };
+    let processed_img = process_image(x_block, y_block, &img).expect("Processing image failed");
+
+    write_output(&out_path, &processed_img).expect("Failed to write to output file");
 
     println!("{}", processed_img);
 }
 
-// Calculate the size of the sub images to average for every ASCII character
+/// Calculate the size of the "sub images", each sub image -> one ASCII character
+/// Note that ASCII is taller than it is wide so ajust the scale accordingly
 fn calculate_block_sizes(scale_factor: f32) -> (u32, u32) {
-    // Calculate the size of the sub images to average, each sub image -> one character
-    // Note that ASCII is taller than it is wide so ajust the scale accordingly
     let mut x_block: u32;
     let mut y_block: u32;
-    if (0.0..1.0).contains(&scale_factor) {
-        // Valid scale factor -> calculate block sizes
+
+    let valid_scale_range = 0.0..1.0;
+    if valid_scale_range.contains(&scale_factor) {
         x_block = (1.0 / scale_factor).floor() as u32;
         y_block = (2.0 / scale_factor).floor() as u32;
-        // Make sure even really small blocks still look right/work at all
+
+        // No zero size sub images
         x_block = if x_block > 0 { x_block } else { 1 };
-        y_block = if y_block > 0 { y_block } else { 2 }; // Height vs width of ASCII
+        y_block = if y_block > 0 { y_block } else { 2 };
     } else {
-        // Invalid scale factor -> manually assign
         x_block = 1;
         y_block = 2;
     }
     (x_block, y_block)
 }
 
-// Write the output to a text file
+/// Write the output to a text file
 fn write_output(path: &String, text: &String) -> Result<(), std::io::Error> {
     if Path::new(path).exists() {
         fs::remove_file(path)?;
@@ -71,7 +55,7 @@ fn write_output(path: &String, text: &String) -> Result<(), std::io::Error> {
     Ok(())
 }
 
-// Takes a brightness from 0 to 255 and maps it to an ascii character.
+/// Takes a brightness from 0 to 255 and maps it to an ascii character
 fn brigtness_to_ascii(brightness: u8) -> Result<char, &'static str> {
     let char_by_color = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
     let indx_float: f32 = (char_by_color.len() as f32 - 1.0) / (255.0) * (brightness as f32);
@@ -82,8 +66,16 @@ fn brigtness_to_ascii(brightness: u8) -> Result<char, &'static str> {
     }
 }
 
-// Takes the block sizes (defines how many pixels are averaged for one ASCII character)
-// and a reference to an image buffer, returns the string representing that image in ASCII
+/// Convert ImageBuffer data to ASCII
+///
+/// # Arguments
+///
+/// * `x_block` - Width in pixels of sub images to be averaged into ASCII characters
+/// * `y_block` - Height in pixels of sub images to be averaged into ASCII characters
+/// * `img` - Image buffer data to process
+///
+/// Takes the block sizes (defines how many pixels are averaged for one ASCII character)
+/// and a reference to an image buffer, returns the string representing that image in ASCII
 fn process_image(
     x_block: u32,
     y_block: u32,
@@ -114,9 +106,8 @@ fn process_image(
             // Average the vec
             let mut average: f32 = 0.0;
             for int in &intensity_vec {
-                average += *int as f32;
+                average += (*int as f32) / (intensity_vec.len() as f32);
             }
-            average /= intensity_vec.len() as f32;
 
             out_file_buf.push(brigtness_to_ascii(average.round() as u8)?);
         }
